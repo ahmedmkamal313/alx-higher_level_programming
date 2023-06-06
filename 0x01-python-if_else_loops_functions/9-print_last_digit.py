b7ccff4f222d0e8986b55a3f5c47931567c2230b
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-06 14:06:32 -0700

    some changed to handle negative number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
+    last_char = str(number)[-1]
+    last_digit = int(last_char)
     print(last_digit, end="")
     return last_digit
