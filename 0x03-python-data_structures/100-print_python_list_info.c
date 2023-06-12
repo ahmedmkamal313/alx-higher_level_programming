c3fb42ad01389ba722622b23b3e585e255d11f6a
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-12 05:34:08 -0700

    added file 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,29 +7,20 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	PyListObject *list;
-	Py_ssize_t size, i;
-	PyObject *item;
+	int size, alloc, i;
+	PyObject *obj;
 
-	/* Check if p is a list object */
-	if (!PyList_Check(p))
-		return;
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
 
-	/* Cast p to a list object */
-	list = (PyListObject *)p;
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
 
-	/* Get the size of the list */
-	size = PyList_Size(p);
-
-	/* Print the size and the allocated space of the list */
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	/* Loop through list and print type and reference count of each element */
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i); /* Get the item at index i */
-		printf("Element %ld: %s\n", i, item->ob_type->tp_name); /* Print type name */
-		printf("  refcount: %ld\n", item->ob_refcnt); /* Print the reference count */
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
