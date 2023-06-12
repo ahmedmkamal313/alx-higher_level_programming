b98a47cfede889dfeacc81f303be87a9cdb2cc1f
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-12 04:19:50 -0700

    added file 11-delete_at.py with updated desc

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,7 +7,9 @@ def delete_at(my_list=[], idx=0):
         for i in range(idx, len(my_list) - 1):
             # shift the elements to the left by one position
             my_list[i] = my_list[i + 1]
-        # remove the last element of the list
-        my_list.pop()
+        # create a new list without the last element using slicing
+        new_list = my_list[:-1]
+        # assign the new list back to the original list
+        my_list = new_list
     # return the modified list
     return my_list
