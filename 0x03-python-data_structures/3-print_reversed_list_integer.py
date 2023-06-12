3ffd4f72bb62e50b00d4f246ee61ed3bde6d13db
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-12 03:03:29 -0700

    edit in file 3 for mysterious output

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     # loop through my_list using negative idexing.
-    for i in range(-1, -len(my_list) - 1, -1):
-        # print each integer using str.format()
+    for i in range(len(my_list) - 1, -1, -1):
+        # print the current element using str.format()
         print("{:d}".format(my_list[i]))
