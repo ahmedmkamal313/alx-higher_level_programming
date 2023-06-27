541a39b04bb1cfa0b3c450ef05f0624f3a31dfed
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-06-27 02:18:38 -0700

    fix pycodestyle errors

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/python3
 """This module defines a class Square"""
 
+
 class Square:
-    """This class represents a square with a private instance attribute: size"""
+    """This class represents a square with private instance attribute: size"""
 
     def __init__(self, size):
         """Initializes the square with the given size"""
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class Square"""
 
+
 class Square:
-    """This class represents a square with a private instance attribute: size"""
+    """This class represents a square with private instance attribute: size"""
 
     def __init__(self, size=0):
         """Initializes the square with the given size"""
