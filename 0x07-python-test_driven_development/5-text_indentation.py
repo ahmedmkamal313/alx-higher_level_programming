5a1a4e8e2fa7e7dad2079650eb407827285ac500
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-07-07 13:45:02 -0700

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
-""" text_indentation returns "text" in the specified format:
-2 newlines after each ['.', '?', ':']
-"""
+"""module text_indentation, Adds two new lines after a set of characters."""
 
 
-# Define a function that takes one parameter: a text string
 def text_indentation(text):
-    """ prints "text" with 2 newlines after each of these char: ['.', '?', ':']
-    checks if "text" is a str
-    first loop removes spaces after each required chars
-    second loop adds 2 newlines after each required chars
-    """
+    """Prints text with added two newlines after
+    each of these characters {'.', '?', ':'}."""
 
-    # Raise a TypeError if text is not a string
-    if type(text) != str:
+    # Check if the input is a string, otherwise raise an error
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    # Define a list of characters that trigger a new line
-    special_chars = ['.', '?', ':']
-
-    # Removes the space after special chars
-    index = 0
-    for char in text:
-        if char in special_chars:
-            if text[index + 1] == " ":
-                # Slice the text to remove the space
-                text = text[:index + 1] + text[index + 2:]
-        else:
-            index += 1
-
-    # Cats '\n\n' after the special char with removed space
-    index = 0
-    for char in text:
-        if char in special_chars:
-            # Slice the text to insert two new lines
-            text = text[:index + 1] + '\n\n' + text[index + 1:]
-            index += 3
-        else:
-            index += 1
+    # Loop through the special characters that trigger a new line
+    for delim in ".:?":
+        # Split the text by the current delimiter
+        # and remove any spaces after it
+        # Then join the text segments with the delimiter and two new lines
+        text = (delim + "\n\n").join(
+                [line.strip(" ") for line in text.split(delim)])
 
     # Print the formatted text without an extra new line at the end
-    print(text, end='')
+    print("{}".format(text), end="")
