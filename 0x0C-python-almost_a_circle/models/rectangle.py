3bee0a6650d92f70faf4d5c1062b85185400dd6a
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-07-16 19:09:31 +0300

    added class rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A class that represents a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a Rectangle instance with the given attributes"""
+
+        # call the super class with id
+        super().__init__(id)
+
+        # assign each argument to the right attribute
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+        @property
+        def width(self):
+            """The getter for the width attribute"""
+
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """The setter for the width attribute"""
+
+            # Validate the value as an integer greater than zero
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be > 0")
+
+            # Assign the value to the private attribute
+            self.__width = value
+
+        @property
+        def height(self):
+            """The getter for the height attribute"""
+
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """The setter for the height attribute"""
+
+            # Validate the value as an integer greater than zero
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be < 0")
+
+            # Assign the value to the private attribute
+            self.__height = value
+
+
+        @property
+        def x(self):
+            """The getter for the x attribute"""
+
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            """The setter for the x attribute"""
+
+            # Validate the value as an integer greater than or equal to zero
+            if type(value) is not int:
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError("x must be >= 0")
+
+            # Assign the value to the private attribute
+            self.__x = value
+
+        @property
+        def y(self):
+            """The getter for the y attribute"""
+
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            """The setter for the y attribute"""
+
+            # Validate the value as an integer greater than or equal to zero
+            if type(value) is not int:
+                raise TypeError("y must be an integer")
+            if value < 0:
+                raise ValueError("y must be >= 0")
+
+            # Assign the value to the private attribute
+            self.__y = value 
