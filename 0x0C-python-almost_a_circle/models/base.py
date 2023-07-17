5e4cb4e8d44a5775149358c2322c910a067c441c
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-07-17 14:50:27 +0300

    updating

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 
 
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -73,3 +75,91 @@ class Base:
         dummy.update(**dictionary)
 
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances from a file"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as f:
+                data = f.read()
+                list_dict = cls.from_json_string(data)
+                list_inst = []
+                for d in list_dict:
+                    list_inst.append(cls.create(**d))
+                return list_inst
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes a list of objects to a CSV file"""
+
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w") as f:
+            writer = csv.writer(f)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([obj.id, obj.width, obj.height,
+                                     obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes a list of objects from a CSV file"""
+
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r") as f:
+                reader = csv.reader(f)
+                list_dict = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        list_dict.append({"id": int(row[0]),
+                                          "width": int(row[1]),
+                                          "height": int(row[2]),
+                                          "x": int(row[3]),
+                                          "y": int(row[4])})
+                    elif cls.__name__ == "Square":
+                        list_dict.append({"id": int(row[0]),
+                                          "size": int(row[1]),
+                                          "x": int(row[2]),
+                                          "y": int(row[3])})
+                list_inst = []
+                for d in list_dict:
+                    list_inst.append(cls.create(**d))
+                return list_inst
+        except FileNotFoundError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draws all the Rectangles and Squares using Turtle graphics"""
+
+        t = turtle.Turtle()
+        t.speed(0)
+        t.hideturtle()
+        for r in list_rectangles:
+            t.penup()
+            t.goto(r.x, r.y)
+            t.pendown()
+            t.color("red")
+            t.begin_fill()
+            for i in range(2):
+                t.forward(r.width)
+                t.left(90)
+                t.forward(r.height)
+                t.left(90)
+            t.end_fill()
+        for s in list_squares:
+            t.penup()
+            t.goto(s.x, s.y)
+            t.pendown()
+            t.color("blue")
+            t.begin_fill()
+            for i in range(4):
+                t.forward(s.size)
+                t.left(90)
+            t.end_fill()
+        turtle.done()
