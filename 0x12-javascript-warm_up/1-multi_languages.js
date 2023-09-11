dc2966d8ca2e49c1398024e65a3da37d00b56094
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-09-11 15:47:00 +0300

    fix an error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+// Declare 3 constant variables with the values of the lines to print
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+// Use console.log(...) to print each line
+console.log(line1);
+console.log(line2);
+console.log(line3);
