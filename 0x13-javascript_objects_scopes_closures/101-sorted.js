0309896b3f9214c41c7c56e9df9350fe915aeb3d
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-09-12 21:58:09 +0300

    fix error

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-// A script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
-// The dict object contains some key-value pairs of numbers
 exports.dict = {
-  89: 1, // The key 89 has the value 1
-  90: 2, // The key 90 has the value 2
-  91: 1, // The key 91 has the value 1
-  92: 3, // The key 92 has the value 3
-  93: 1, // The key 93 has the value 1
-  94: 2 // The key 94 has the value 2
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
 };
