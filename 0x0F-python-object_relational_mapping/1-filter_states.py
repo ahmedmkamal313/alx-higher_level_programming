fdc359d30a4bf7b88d3436fca939033942822e48
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-09-17 19:04:09 +0300

    fix some errors and dded new files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,35 +4,8 @@ import MySQLdb
 # Import sys module to get command line arguments
 import sys
 
-# Get the arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-# Connect to the MySQL server
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-        )
-
-# Create a cursor object
-cursor = db.cursor()
-
-# Execute the SQL query to select all states with a name starting with N
-cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-        )
-
-# Fetch all the rows from the cursor
-rows = cursor.fetchall()
-
-# Loop through the rows and print each state
-for row in rows:
-    print(row)
-
-# Close the cursor and the database connection
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,40 +4,8 @@ import MySQLdb
 # Import sys module to get command line arguments
 import sys
 
-# Get the arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-state_name = sys.argv[4]
-
-# Connect to the MySQL server
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-        )
-
-# Create a cursor object
-cursor = db.cursor()
-
-# Use format to create the SQL query with the user input
-query = (
-        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
-        .format(state_name)
-        )
-
-# Execute the query
-cursor.execute(query)
-
-# Fetch all the rows from the cursor
-rows = cursor.fetchall()
-
-# Loop through the rows and print each state
-for row in rows:
-    print(row)
-
-# Close the cursor and the database connection
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
