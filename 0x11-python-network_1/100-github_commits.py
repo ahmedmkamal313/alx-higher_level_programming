b764f155a8d545d48be6611146dc795a38b488bc
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-12-01 14:23:01 -0800

    try another fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """
-This module takes in a URL and an email address, sends a POST request to
-http://0.0.0.0:5000/search_user with the email as a parameter using the
-requests and sys packages. It also displays the id and name of the user
-found or an error message if the response is not valid or empty.
+This module takes a repository name and an owner name and lists 10 commits
+(from the most recent to oldest) of the repository by the user, using the
+GitHub API and the requests package.
 """
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    # Get the URL and email address from the command line arguments
-    url = sys.argv[1]
-    email = sys.argv[2]
-    # Send a POST request to the given URL with the email as a parameter
-    response = requests.post(url, data={'email': email})
-    # Get the response body as a string
-    html = response.text
-    # Print the response body
-    print(html)
+    # Get the repository name& the owner name from the command line arguments
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    # Set the URL for the GitHub API with the given parameters
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    # Send a GET request to the URL
+    response = requests.get(url)
+    # Get the JSON data from the response
+    commits = response.json()
+    # Try to loop through the first 10 items of the data
+    try:
+        for i in range(10):
+            # Print the sha and the author name for each commit
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    # Handle the exception if the data has less than 10 items
+    except IndexError:
+        pass
